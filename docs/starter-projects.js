51c7a5b76fccd0fb8f01f36bdaf561da3948eba3
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-10-11 16:51:34 +0200

    add started projects

---
+++ docs/starter-projects.js
@@ -0,0 +1,19 @@
+
+// nextjs 15 templates
+https://github.com/Saas-Starter-Kit/Saas-Kit-prisma
+https://github.com/timlrx/tailwind-nextjs-starter-blog
+https://github.com/onwidget/tailnext
+https://github.com/NextJSTemplates/startup-nextjs
+https://github.com/NextJSTemplates/solid-nextjs
+https://github.com/NextJSTemplates/play-nextjs
+https://github.com/Skolaczk/next-starter
+https://github.com/hadrysm/nextjs-boilerplate
+https://github.com/Blazity/next-enterprise
+https://github.com/ghbishal/nextjs-boilerplate
+https://github.com/shadcn-ui/next-template
+-----
+// server actions, drizzle
+https://github.com/leerob/next-saas-starter
+-----
+// tests
+https://github.com/theodorusclarence/ts-nextjs-tailwind-starter
--- docs/todo1.md
+++ docs/todo1.md
@@ -1,1 +1,3 @@
-
+poenta, sad gotov app upotrebljiv, a ne template
+---
+izaberi koji projekat da iskopiras u folder
